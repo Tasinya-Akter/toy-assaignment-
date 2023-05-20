274e416215c559fb1c85feaa3fb35833205fdaf4
Al Amin <alamin>
2023-05-20 11:16:09 +0600

    extra section one

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,14 +5,16 @@ import Banner from './Banner';
 import Gallery from './Gallery';
 import DollTab from './DollTab';
 import useTitle from '../../Hooks/useTitle';
+import Unleas from './Unleas';
 
 const Home = () => {
   useTitle("Home");
   return (
     <div className='container mx-auto py-[60px] px-3 md:px-0'>
       <Banner></Banner>
-      <Gallery></Gallery>
       <DollTab></DollTab>
+      <Unleas></Unleas>
+      <Gallery></Gallery>
     </div>
   )
 }
---
+++ src/Pages/Home/Unleas.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Unleas = () => {
+  return (
+    <div className='py-12'>
+        <div className="container mx-auto">
+            <span className='text-4xl font-Bubblegum text-primary'>Unleash the Power of Play with Toytopia</span>
+            <div className='mt-8 border-l-2 border-primary pl-2'>
+                <p className='font-Jost mb-5 text-lg'>Welcome to Toytopia, your ultimate destination for all things toys and playtime! We are passionate about creating a world where imagination knows no bounds and where children can explore, learn, and have fun through play.</p>
+
+                <p className='font-Jost mb-5 text-lg'>At Toytopia, we believe that toys play a crucial role in a child's development, fostering creativity, cognitive skills, and social interactions. Our mission is to provide a diverse range of high-quality toys that spark joy, inspire imaginative play, and contribute to the growth and happiness of children.</p>
+
+                <p className='font-Jost mb-5 text-lg'>What sets us apart is our carefully curated selection of toys from renowned brands, ensuring that every product meets our strict standards of quality, safety, and play value. We understand the importance of offering toys that engage and entertain children of all ages and interests. Whether you're looking for educational toys to encourage learning, interactive toys for hours of entertainment, or classic toys that evoke nostalgia, we have something for everyone.</p>
+
+                <p className='font-Jost mb-5 text-lg'>Thank you for choosing Toytopia as your go-to toy destination. Join us in creating memorable playtime moments and nurturing the imagination and wonder of children everywhere.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Unleas
